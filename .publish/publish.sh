d8a4c50aad00bf7e679369119fb57d1d0008d08b
Hyper_ <40342021+NotHyper-474@users.noreply.github.com>
2026-02-25 16:28:09 -0300

    Majorly improve publish script to resolve correct path

--- .publish/publish.sh
+++ .publish/publish.sh
@@ -1,8 +1,8 @@
 # Handy script to pack everything in a ZIP
 
-#cd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$( cd $( dirname -- "${BASH_SOURCE[0]}" )/.. && pwd)"
 
-ignoreList=("LICENSE" ".gitignore" "*.zip")
+ignoreList=("LICENSE.txt" ".gitignore" "*.zip")
 
 rm mobileDebugDisplay.zip &> /dev/null
 zip mobileDebugDisplay.zip * -x ${ignoreList[@]}
